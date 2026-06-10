dbfa2ec1af3d3099a51d08251e671fab0370fa54
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2026-06-10 14:54:08 +0300

    proto contrac now from separated monorepo

--- internal/handlers/userHandler.go
+++ internal/handlers/userHandler.go
@@ -3,14 +3,14 @@ package handlers
 import (
 	"context"
 
-	userpb "github.com/HackMateGolang/AuthService/api/proto/v1"
+	authpb "github.com/HackMateGolang/proto-contracts/gen/go/auth/v1"
 	"github.com/HackMateGolang/AuthService/internal/models"
 	"github.com/HackMateGolang/AuthService/internal/service"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type UserHandler struct {
-	userpb.UnimplementedAuthServiceServer
+	authpb.UnimplementedAuthServiceServer
 	service *service.UserService
 }
 
@@ -18,24 +18,24 @@ func NewUserHandler(service *service.UserService) *UserHandler {
 	return &UserHandler{service: service}
 }
 
-func (h *UserHandler) CreateUser(ctx context.Context, req *userpb.CreateUserRequest) (*userpb.CreateUserResponse, error) {
+func (h *UserHandler) CreateUser(ctx context.Context, req *authpb.CreateUserRequest) (*authpb.CreateUserResponse, error) {
 	user, err := h.service.CreateUser(ctx, &models.UserCreateRequest{Login: req.Login, Email: req.Email, Password: req.Password})
 	if err != nil {
 		return nil, err
 	}
 
-	return &userpb.CreateUserResponse{
+	return &authpb.CreateUserResponse{
 		Token: user,
 	}, nil
 }
 
 
-func (h *UserHandler) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
+func (h *UserHandler) GetUser(ctx context.Context, req *authpb.GetUserRequest) (*authpb.GetUserResponse, error) {
 	user, err := h.service.GetUser(ctx, &models.UserGetRequest{Login: req.Login, Email: req.Email})
 	if err != nil {
 		return nil, err
 	}
-	return  &userpb.GetUserResponse{
+	return  &authpb.GetUserResponse{
 		Login: user.Login,
 		Email: user.Email,
 		PasswordHash: user.PasswordHash,
@@ -44,7 +44,7 @@ func (h *UserHandler) GetUser(ctx context.Context, req *userpb.GetUserRequest) (
 		CreatedAt: timestamppb.New(user.CreatedAt),
 	},nil
 }
-func (h *UserHandler) PatchUser(ctx context.Context, req *userpb.PatchUserRequest) (*userpb.PatchUserResponse, error) {
+func (h *UserHandler) PatchUser(ctx context.Context, req *authpb.PatchUserRequest) (*authpb.PatchUserResponse, error) {
 	createdAtTime := req.CreatedAt.AsTime()
 	ok, err := h.service.PatchUser(ctx, &models.UserPatchRequest{
 		Login: req.Login,
@@ -57,9 +57,9 @@ func (h *UserHandler) PatchUser(ctx context.Context, req *userpb.PatchUserReques
 		if err != nil {
 		return nil, err
 	}
-	return &userpb.PatchUserResponse{Ok: ok}, nil
+	return &authpb.PatchUserResponse{Ok: ok}, nil
 }
-func (h *UserHandler) PutUser(ctx context.Context, req *userpb.PutUserRequest) (*userpb.PutUserResponse, error) {
+func (h *UserHandler) PutUser(ctx context.Context, req *authpb.PutUserRequest) (*authpb.PutUserResponse, error) {
 	createdAtTime := req.CreatedAt.AsTime()
 	ok, err := h.service.PutUser(ctx, &models.UserPutRequest{
 		Login: req.Login,
@@ -73,15 +73,14 @@ func (h *UserHandler) PutUser(ctx context.Context, req *userpb.PutUserRequest) (
 		return nil, err
 	}
 
-	return &userpb.PutUserResponse{Ok: ok}, nil
+	return &authpb.PutUserResponse{Ok: ok}, nil
 }
-func (h *UserHandler) DeleteUser(ctx context.Context, req *userpb.DeleteUserRequest) (*userpb.DeleteUserResponse, error) {
+func (h *UserHandler) DeleteUser(ctx context.Context, req *authpb.DeleteUserRequest) (*authpb.DeleteUserResponse, error) {
 	ok, err := h.service.DeleteUser(ctx, &models.UserDeleteRequest{
 		Login: req.Login,
-		Email: req.Email,
 	})
 	if err != nil {
-		return &userpb.DeleteUserResponse{Ok: ok}, err
+		return &authpb.DeleteUserResponse{Ok: ok}, err
 	}
-	return &userpb.DeleteUserResponse{Ok: ok}, nil
+	return &authpb.DeleteUserResponse{Ok: ok}, nil
 }
